42c16fa4484d234500a61c77edf4755935c238bb
Julian Leon <juliandgleon@gmail.com>
2022-09-06 17:25:22 -0500

    Added update function

--- src/App.svelte
+++ src/App.svelte
@@ -10,7 +10,32 @@
   let products = [];
   let defaultProduct = { id: '', title:'', description:'', state: false };
   let product = {...defaultProduct};
+  let myLocalStorage = localStorage.getItem( 'products' );
 
+  // LOCAL STORAGE ACTIONS
+  if ( myLocalStorage ) { products = JSON.parse( myLocalStorage ) } // GET
+  $: localStorage.setItem( 'products', JSON.stringify( products ) ); // SAVE - PUSH
+
+  let toastLive;
+  let toastInstancia;
+  let toastActions = { name:'', action:'' };
+
+  $: {
+    if ( toastLive ) {
+      toastInstancia = new bootstrap.Toast( toastLive, { delay: 3000 } );
+    }
+  }
+
+  // Toast function
+  function showToast( params={ action:'', name:'' } ) {
+    toastActions.name = params.name;
+    toastActions.action = params.action;
+
+    // Toast
+    toastInstancia.show();
+  }
+
+  // Add
   function addProduct() {
     if ( product.title === '' ) {
       console.log( 'The task must have full content!' );
@@ -20,23 +45,41 @@
     product.id = uuidv4();
     products = [...products, product];
 
-    // Toast
-    const toastLive = document.getElementById( 'liveToast' );
-    const toast = new bootstrap.Toast( toastLive, {
-      delay: 3000
-    } );
-    toast.show();
-
     // Reset product
+    const currentProduct = product;
     product = {...defaultProduct};
+
+    showToast( {
+      name: currentProduct.title,
+      action: 'created'
+    } );
   }
 
   // Delete
   function deleteProduct( id ) {
+    const currentProduct = products.find( ( item ) => item.id === id );
     const newProducts = products.filter( ( item ) => item.id !== id );
     products = [...newProducts];
+
+    showToast( {
+      name: currentProduct.title,
+      action: 'deleted'
+    } );
   }
 
+  // Update
+  function updateProduct( id ) {
+    const currentProduct = products.find( ( item ) => item.id === id );
+    products = products.map( ( item ) => item.id === id ? { ...item, state: !item.state } : item );
+
+    showToast( {
+      name: currentProduct.title,
+      action: 'Updated'
+    } );
+  }
+
+  const approveIconClass = ( state ) => ( state ? 'bi bi-arrow-clockwise' : 'bi bi-check2' );
+  const approveColorClass = ( state ) => ( state ? 'btn btn-danger' : 'btn btn-success' );
 </script>
 
 <main>
@@ -45,13 +88,13 @@
 
     <!-- Toast -->
     <div class="toast-container position-fixed bottom-0 end-0 p-3">
-      <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+      <div bind:this={ toastLive } id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
         <div class="toast-header">
           <strong class="me-auto">CRUD</strong>
           <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
         <div class="toast-body">
-          The task <b>{product.title}</b> was created!
+          The product <b>{ toastActions.name }</b> was { toastActions.action }!
         </div>
       </div>
     </div>
@@ -84,15 +127,22 @@
     </form>
 
     <div>
-      <h1></h1>
+      <!-- <h1></h1> -->
       {#each products as item}
         <div class="card my-2">
           <div class="card-body">
-            <h5 class="card-title">{item.title}</h5>
+            <h5 class="card-title { item.state ? 'text-decoration-line-through' : '' }">{item.title}</h5>
+
             <p class="card-text">{item.description}</p>
-            <button type="button" class="btn btn-warning">Edit</button>
-            <button type="button" class="btn btn-success">Done</button>
-            <button type="button" on:click={ deleteProduct( item.id ) } class="btn btn-danger">Delete</button>
+
+            <button type="button" class="btn btn-warning"><i class="bi bi-pencil"></i> Edit</button>
+
+            <button type="button" class={ approveColorClass( item.state ) } on:click={ updateProduct( item.id ) } >
+              <i class={ approveIconClass( item.state ) }></i>
+              { item.state ? 'Undo' : 'Done' }
+            </button>
+
+            <button type="button" on:click={ deleteProduct( item.id ) } class="btn btn-danger"><i class="bi bi-trash"></i> Delete</button>
           </div>
         </div>
       {/each}
