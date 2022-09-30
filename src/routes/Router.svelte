4b3c0a8d7ecc721aaed8b625e88989a16642c47e
Julian Leon <juliandgleon@gmail.com>
2022-09-30 17:05:26 -0500

    Add router

---
+++ src/routes/Router.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { Router, Link, Route } from 'svelte-routing';
+
+  // Routes
+  // import Crud from './Crud.svelte';
+  // import Home from './Home.svelte';
+
+  // Views
+  import Home from '../views/Home.svelte';
+  import Create from '../views/Create.svelte';
+  import Login from '../views/Login.svelte';
+  import SignIn from '../views/SignIn.svelte';
+  import List from '../views/List.svelte';
+  import Account from '../views/Account.svelte';
+</script>
+
+<!-- Routing -->
+<Router>
+  <!-- <Route path="/crud" component={ Crud } /> -->
+  <Route path="/"> <Home /> </Route>
+  <Route path="/login"> <Login /> </Route>
+  <Route path="/signin"> <SignIn /> </Route>
+  <Route path="/account"> <Account /> </Route>
+  <Route path="/create"> <Create /> </Route>
+  <Route path="/list"> <List /> </Route>
+</Router>
