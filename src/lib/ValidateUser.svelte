f33f814f6e4b55c39cea9ce18ae03de082781729
Julian Leon <juliandgleon@gmail.com>
2022-09-30 17:07:27 -0500

    Add user validation

---
+++ src/lib/ValidateUser.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { onMount } from 'svelte';
+  import { navigate } from 'svelte-routing';
+  import { user } from '../stores/stores';
+
+  onMount( () => {
+    if ( !$user ) {
+      navigate( '/login', {
+        replace: true
+      } );
+    }
+  } );
+</script>
